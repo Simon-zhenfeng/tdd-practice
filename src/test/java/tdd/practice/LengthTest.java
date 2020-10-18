15087d4fea62051f2a73ae499145a0af98ef5047
simon <hanzhenfeng@wuxicloud.com>
2020-10-18 11:52:50 +0800

    refactor ago

---
+++ src/test/java/tdd/practice/LengthTest.java
@@ -0,0 +1,44 @@
+package tdd.practice;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Simon
+ * @date 2020/10/18 11:19
+ */
+public class LengthTest {
+    @Test
+    public void should_create_length_with_amount_and_unit() {
+        Length inch = new Length(1, "inch");
+        assertThat(inch.amount()).isEqualTo(1);
+        assertThat(inch.unit()).isEqualTo("inch");
+    }
+
+    @Test
+    public void should_1_inch_equal_to_1_inch() {
+        Length inch = new Length(1, "inch");
+        assertThat(inch).isEqualTo(new Length(1, "inch"));
+    }
+
+    @Test
+    public void should_display_inner_info_format() {
+        assertThat(new Length(1, "inch").toString()).isEqualTo("1 (inch)");
+    }
+
+    @Test
+    public void should_1_foot_equal_to_12_inchs() {
+        assertThat(new Length(1, "foot")).isEqualTo(new Length(12, "inch"));
+    }
+
+    @Test
+    public void should_1_yard_equal_to_3_feet() {
+        assertThat(new Length(1, "yard")).isEqualTo(new Length(3, "foot"));
+    }
+
+    @Test
+    public void should_1_yard_equal_to_36_inchs() {
+        assertThat(new Length(1, "yard")).isEqualTo(new Length(36, "inch"));
+    }
+}
